10654dfefcc9f9303796633e01d0cdb317632e0e
damiapaw99 <damianpaw99@gmail.com>
2021-01-04 14:57:13 +0100

    Added base of the program

---
+++ src/main/java/edu/ib/Controller.java
@@ -0,0 +1,4 @@
+package edu.ib;
+
+public class Controller {
+}
---
+++ src/main/java/edu/ib/CrazyEights.java
@@ -0,0 +1,24 @@
+package edu.ib;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CrazyEights extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/crazy_eights.fxml"));
+        Scene scene=new Scene(root,1000,700);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
